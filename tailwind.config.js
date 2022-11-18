7beb5f39abc030066edf0868828a501b5ccf9a42
Luis Sequera <luissequeracontreras@gmail.com>
2022-11-18 12:37:21 -0500

    create 4 DIV and fix header

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,14 +14,23 @@ module.exports = {
       },
       fontFamily: {
         'sans': "'Inter', sans-serif",
+        'epilogue': "'Epilogue', sans-serif",
         'cascadia': '"CascadiaCode"',
       },
       colors: {
         'do-blue-dark': '#080c2d',
+        'do-blue-dark2': 'rgb(8, 27, 75)',
+        'do-blue-dark3': 'rgb(3, 27, 78)',
         'do-blue-mediumdark': '#1d274c',
         'do-blue-mediumdark2': 'rgb(36, 51, 90)',
         'do-blue-medium': 'rgb(20, 86, 255)',
         'do-blue-light': 'rgb(0, 105, 255)',
+        'do-gray-medium': 'rgb(77, 91, 124)',
+        'do-white': 'rgb(249, 250, 254)',
+      },
+      boxShadow: {
+        'input': '0 5px 1px 0 rgb(0, 0, 0, 0.1)',
+        'input-focus': '0 2px 1px 0 rgb(0, 0, 0, 0.1)',
       },
     },
   },
